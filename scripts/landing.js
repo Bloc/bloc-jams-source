0a17805e0d9f278ef866170e4b429a1ecacf8cd2
Colleen <colleen@bloc.io>
2015-09-28 16:59:58 -0700

    24. DOM Scripting: Collection View
    Note that the lines removed from index.html, landing.js as well as
    removing utilities.js altogether are because the previous checkpoint’s
    assignment should NOT have been merged. These lines should not be part
    of the application from this point on. The code in landing.js reverts
    back to the code from the previous checkpoint, prior to the assignment.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,16 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
-};
-
-var animatePoints = function(points) {    
-    forEach(points, revealPoint);  
+var animatePoints = function(points) {
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";   
+    };
+    
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
 };
 
 window.onload = function() {
--- scripts/utilities.js
+++
@@ -1,5 +0,0 @@
-function forEach(array, callback) {
-    for (var i = 0; i < array.length; i++) {
-        callback(array[i]);      
-    }
-}
