228f8dcec12c52dfbc6972c09b5639d753242db2
Colleen <colleen@bloc.io>
2015-09-28 16:39:59 -0700

    23. DOM Scripting: Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
-    
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
     var revealPoint = function(index) {
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
@@ -12,3 +12,18 @@ var animatePoints = function() {
         revealPoint(i);
     }
 };
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener("scroll", function(event) {
+        if (document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);   
+        }
+    });
+}
