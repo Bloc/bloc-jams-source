951dedee86e1f00c9bbc6a18f5f2d7e9f854f918
Colleen <colleen@bloc.io>
2015-09-28 16:45:31 -0700

    23. DOM Scripting: Events – Assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";   
-    };
-    
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function(points) {    
+    forEach(points, revealPoint);  
 };
 
 window.onload = function() {
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+function forEach(array, callback) {
+    for (var i = 0; i < array.length; i++) {
+        callback(array[i]);      
+    }
+}
