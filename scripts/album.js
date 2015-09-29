4cc91038e27885a71afbdca07e98c2f95262be07
Colleen <colleen@bloc.io>
2015-09-29 11:58:25 -0700

    28. HTML & CSS: Music Player
    Note that the code in album.js reverts back to the code from the
    previous checkpoint because the previous checkpoint’s assignment should
    NOT have been merged. These lines should not be part of the application
    from this point on.

--- scripts/album.js
+++ scripts/album.js
@@ -59,28 +59,13 @@ var setCurrentAlbum = function(album) {
     }
 };
 
-var child = document.getElementsByClassName('album-view-title')[0];
-var noParent = document.querySelector('html');
-
 var findParentByClassName = function(element, targetClass) {
     var currentParent = element.parentElement;
-    
-    if (currentParent) {
-        while (currentParent.className && currentParent.className != targetClass) {
-            currentParent = currentParent.parentElement;
-        }
-        
-        if (currentParent.className == targetClass) {
-            return currentParent;
-        } else {
-            alert("No parent with that class name found.");
-        }
-    } else {
-        alert("No parent found.");
+    while (currentParent.className != targetClass) {
+        currentParent = currentParent.parentElement;
     }
+    return currentParent;
 };
-
-findParentByClassName(child, 'album-view');
 
 var getSongItem = function(element) {  
     switch (element.className) {
