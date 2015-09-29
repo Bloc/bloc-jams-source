3241dfb0e5c5f20b2b67acabde8b7152e7c213be
Colleen <colleen@bloc.io>
2015-09-28 17:22:27 -0700

    26. DOM Scripting: Play/Pause Part 1
    Note that many of the lines removed from album.js are because the
    previous checkpoint’s assignment should NOT have been merged. These
    lines should not be part of the application from this point on.

--- scripts/album.js
+++ scripts/album.js
@@ -28,25 +28,10 @@ var albumMarconi = {
     ]
 };
 
-var albumCher = {
-    name: 'Love Hurts',
-    artist: 'Cher',
-    label: 'Imperial',
-    year: '1991',
-    albumArtUrl: 'assets/images/album_covers/16.png',
-    songs: [
-        { name: 'Save Up All Your Tears', length: '4:00' },
-        { name: 'Love Hurts', length: '4:19' },
-        { name: 'Love and Understanding', length: '3:21' },
-        { name: 'Fires of Eden', length: '3:14' },
-        { name: 'I\'ll Never Stop Loving You', length: '2:15' }
-    ]
-};
-
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -55,13 +40,13 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
-var albumTitle = document.getElementsByClassName('album-view-title')[0];
-var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-var albumImage = document.getElementsByClassName('album-cover-art')[0];
-var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-
 var setCurrentAlbum = function(album) {
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
     albumTitle.firstChild.nodeValue = album.name;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -74,16 +59,23 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
-
-    var albums = [albumPicasso, albumMarconi, albumCher];
-    var index = 1;
-    albumImage.addEventListener("click", function(event) {
-        setCurrentAlbum(albums[index]);
-        index++;
-        if (index == albums.length) {
-            index = 0;
+    
+    songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
         }
-    }); 
+    });
+    
+    for (i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+    }
 };
